0557d25518b8b622e5e95a8ee5e6ad5e024818b9
mh <mh@immerda.ch>
2009-07-06 10:12:19 +0200

    set a global passenger buffer dir
    with passenger 2.2.4 an additional buffer directroy have
    been added, which have to be writeable by the apache process
    running as apache user, as it is seperated from the application
    process as used for fileuploads.

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -4,4 +4,17 @@ class passenger::apache{
     ensure => installed,
     require => Package['apache'],
   }
+
+  file{'/var/www/passenger_buffer':
+    ensure => directory,
+    require => [ Package['apache'], Package['mod_passenger'] ],
+    owner => apache, group => 0, mode => 0600;
+  }
+
+  file{'/etc/httpd/conf.d/mod_passenger_custom.conf':
+    content => "PassengerUploadBufferDir /var/www/passenger_buffer\n",
+    require => File['/var/www/passenger_buffer'],
+    notify => Service['apache'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
