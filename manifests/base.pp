39d499fcb04b3c920f64fc5cc1d8a0933f0a09e4
Micah Anderson <micah@riseup.net>
2012-06-27 22:18:03 -0400

    2.7 underscore rename

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,8 +4,8 @@ class passenger::base {
   
   apache::config::file { 'mod_passenger':
     ensure => present,
-    source => [ "puppet://${server}/modules/site-passenger/${fqdn}/mod_passenger.conf",
-                "puppet://${server}/modules/site-passenger/mod_passenger.conf",
+    source => [ "puppet://${server}/modules/site_passenger/${fqdn}/mod_passenger.conf",
+                "puppet://${server}/modules/site_passenger/mod_passenger.conf",
                 "puppet://${server}/modules/passenger/mod_passenger.conf",
               ],
   }
