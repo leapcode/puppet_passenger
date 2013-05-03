33aa73e535aa586fe618a2d414de12327198f7e0
Micah Anderson <micah@riseup.net>
2013-05-03 12:01:46 -0400

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,15 +13,15 @@ class passenger (
   $librack_ensure_version = 'installed',
   $passenger_bin_path = '/usr/sbin' )
 {
-  include apache
+  include ::apache
 
   if ! $use_gems {
     apache::module { 'passenger':
-      ensure => $passenger_ensure_version,
+      ensure       => $passenger_ensure_version,
       package_name => 'libapache2-mod-passenger';
     }
 
-    if !defined(Package["librack-ruby"]) {
+    if !defined(Package['librack-ruby']) {
       if $::lsbdistcodename == 'squeeze' {
         package { 'librack-ruby1.8': ensure => $librack_ensure_version }
       }
@@ -34,38 +34,39 @@ class passenger (
   }
   else {
     package {
-      "passenger":
+      'passenger':
+        ensure   => $passenger_ensure_version;
         provider => gem,
-        ensure => $passenger_ensure_version;
-      "rack":
+      'rack':
+        ensure   => $librack_ensure_version;
         provider => gem,
-        ensure => $librack_ensure_version;
     }
   }
 
   apache::config::file { 'mod_passenger':
     ensure => present,
-    source => [ "puppet://${server}/modules/site_passenger/${fqdn}/mod_passenger.conf",
-                "puppet://${server}/modules/site_passenger/mod_passenger.conf",
-                "puppet://${server}/modules/passenger/mod_passenger.conf",
+    source => [ "puppet:///modules/site_passenger/${::fqdn}/mod_passenger.conf",
+                'puppet:///modules/site_passenger/mod_passenger.conf',
+                'puppet:///modules/passenger/mod_passenger.conf',
               ],
   }
 
   if $use_munin {
-    case $passenger_memory_munin_config { '':
-      { $passenger_memory_munin_config = "user root\nenv.passenger_memory_stats $passenger_bin_path/passenger-memory-stats" }
+    if $passenger_memory_munin_config == '' {
+      $passenger_memory_munin_config = "user root\nenv.passenger_memory_stats $passenger_bin_path/passenger-memory-stats"
     }
 
-    case $passenger_stats_munin_config { '':
-      { $passenger_stats_munin_config = "user root\nenv.passenger_status $passenger_bin_path/passenger-status" }
+    if $passenger_stats_munin_config == '' {
+      $passenger_stats_munin_config = "user root\nenv.passenger_status $passenger_bin_path/passenger-status"
     }
+  }
 
     munin::plugin::deploy {
       'passenger_memory_stats':
-        source => "passenger/munin/passenger_memory_stats",
+        source => 'passenger/munin/passenger_memory_stats',
         config => $passenger_memory_munin_config;
       'passenger_stats':
-        source => "passenger/munin/passenger_stats",
+        source => 'passenger/munin/passenger_stats',
         config => $passenger_stats_munin_config;
     }
   }
