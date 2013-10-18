f7c06a4293fca07c1624546619b9cb43cb66cc4c
Micah Anderson <micah@riseup.net>
2013-10-18 16:31:49 -0400

    lint

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -7,13 +7,13 @@ class passenger::munin {
   case $passenger_stats_munin_config { '':
     { $passenger_stats_munin_config = "user root\n" }
   }
-  
+
   munin::plugin::deploy {
     'passenger_memory_stats':
-      source => "passenger/munin/passenger_memory_stats",
+      source => 'passenger/munin/passenger_memory_stats',
       config => $passenger_memory_munin_config;
     'passenger_stats':
-      source => "passenger/munin/passenger_stats",
+      source => 'passenger/munin/passenger_stats',
       config => $passenger_stats_munin_config;
   }
 
